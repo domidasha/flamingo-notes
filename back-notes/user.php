1ef9f0c829c8df7798ec33364b45e2757b2d0fdc
dasha <daria.stukal@gmail.com>
2016-06-22 17:34:49 +0300

    to single domen

--- back-notes/user.php
+++ back-notes/user.php
@@ -4,13 +4,13 @@ include('functions.php');
 $response['success']='success';
 $flamingo =  new FlamingoListService();
 
-//print_r($flamingo->checkUser('user', '1222'));
+//print_r($flamingo->checkUser('user', '1'));
 
-//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//    if (isset($_POST['login']) and isset($_POST['password'])) {
-//        $login = $_POST['login'];
-//        $password = $_POST['password'];
-//
-//        $flamingo->checkUser($login, $password);
-//    }
-//}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['login']) and isset($_POST['password'])) {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+        $flamingo->checkUser($login, $password);
+    }
+}
--- front-notes/app/app.js
+++ front-notes/app/app.js
@@ -11,3 +11,31 @@
 //.factory('UserService', function ($resource) {
 //    return $resource('http://flamingo-notes.dev/back-notes/notes.php/:id',{id: "@id"});
 //});
+
+
+angular.module('myApp', [
+    'ngRoute', 'ngResource'
+]).config(function ($routeProvider) {
+    $routeProvider.when("/login", {
+        templateUrl: "views/login.html",
+        controller: 'myCtrl'
+    }).when("/settings", {
+        templateUrl: "settings.html",
+        controller: ''
+    }).otherwise({
+        redirectTo: "/"
+    });
+}).controller('myCtrl', function($scope, $http) {
+    $http({
+        method : "GET",
+        url : "/back-notes/notes.php/",
+        params: {
+            id: 2
+        }
+
+    }).then(function mySucces(response) {
+        $scope.myWelcome = response.data;
+    }, function myError(response) {
+        $scope.myWelcome = response.statusText;
+    });
+});
